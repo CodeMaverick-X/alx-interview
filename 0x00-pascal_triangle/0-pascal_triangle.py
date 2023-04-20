3a3c95d6ad7d0735f26dbea7e8877f672c1cc65a
reinhard <reinhard2057@gmail.com>
2023-04-20 21:26:42 +0000

    second trial

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 """pascal's triangle technical interview"""
+from math import factorial
 
 
 def pascal_triangle(n):
-    """python technical interview"""
-    if n <= 0 or not n:
-        return []
+    # """python technical interview"""
+    # if n <= 0 or not n:
+    #   return []
+
+    # lst_int = []
+    #  for num in range(n):
+    #  var = [int(x) for x in list(str(11 ** num))]
+    # lst_int += [var]
+    # return lst_int
 
     lst_int = []
-    for num in range(n):
-        var = [int(x) for x in list(str(11 ** num))]
-        lst_int += [var]
+
+    for i in range(n):
+
+        row = []
+        for j in range(i+1):
+
+            # nCr = n!/((n-r)!*r!)
+            row += [factorial(i)//(factorial(j)*factorial(i-j))]
+        lst_int += [row]
     return lst_int
