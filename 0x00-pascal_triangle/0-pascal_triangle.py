279fddde2e4f27fb578d775ade6c36cc47e27545
reinhard <reinhard2057@gmail.com>
2023-04-20 21:38:26 +0000

    second trial

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,15 +13,3 @@ def pascal_triangle(n):
       var = [int(x) for x in list(str(11 ** num))]
       lst_int += [var]
     return lst_int
-
-    # lst_int = []
-
-    # for i in range(n):
-
-    #     row = []
-    #     for j in range(i+1):
-
-    #         # nCr = n!/((n-r)!*r!)
-    #         row += [factorial(i)//(factorial(j)*factorial(i-j))]
-    #     lst_int += [row]
-    # return lst_int
