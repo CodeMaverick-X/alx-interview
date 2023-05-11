ce9f1bc773615b9ff8d768390d26c0194fa927f8
reinhard <reinhard2057@gmail.com>
2023-05-11 21:35:59 +0000

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,25 +11,26 @@ codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0,
          "500": 0}
 
 pattern = r'^([\d\.]+)\s+-\s+\[(.*?)\]\s+"(.*?)"\s+(\d{3})\s+(\d+)$'
-try:
-    for line in sys.stdin:
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
 
-        match = re.match(pattern, line)
-        if match:
-            file_size += int(line.split()[-1])
-            count += 1
-            code = line.split()[-2]
-            codes[code] += 1
-        if count == 10:
-            print('File size: {}'.format(file_size))
-            for code, c in codes.items():
-                if c > 0:
-                    print('{}: {}'.format(code, c))
-            count = 0
-except KeyboardInterrupt:
-    print('File size: {}'.format(file_size))
-    for code, c in codes.items():
-        if c > 0:
-            print('{}: {}'.format(code, c))
-    count = 0
-    raise KeyboardInterrupt
+            match = re.match(pattern, line)
+            if match:
+                file_size += int(line.split()[-1])
+                count += 1
+                code = line.split()[-2]
+                codes[code] += 1
+            if count == 10:
+                print('File size: {}'.format(file_size))
+                for code, c in codes.items():
+                    if c > 0:
+                        print('{}: {}'.format(code, c))
+                count = 0
+    except KeyboardInterrupt:
+        print('File size: {}'.format(file_size))
+        for code, c in codes.items():
+            if c > 0:
+                print('{}: {}'.format(code, c))
+        count = 0
+        raise KeyboardInterrupt
