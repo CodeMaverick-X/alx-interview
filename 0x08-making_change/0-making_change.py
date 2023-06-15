29fa683bb7afae32f6c426fc4738cc8364bd7420
reinhard <reinhard2057@gmail.com>
2023-06-15 23:13:41 +0000

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+contains func that makes change
+"""
+
+
+def makeChange(coins, total):
+    """makes cahnge with the fewest coin to make
+    up total"""
+
+    if total <= 0:
+        return 0
+    sort_coin = sorted(coins, reverse=True)
+    len_c = len(coins)
+    count = 1
+    val = 0
+    log = 0
+    new_val = 0
+
+    while val < len_c:
+        if (count * sort_coin[val] + new_val) > total:
+            log += count - 1
+            new_val += sort_coin[val] * (count - 1)
+            val += 1
+            count = 1
+        else:
+            count += 1
+        if new_val == total:
+            return log
+    return -1
